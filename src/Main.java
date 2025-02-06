23a5ff1dd2d6fe1a105c60b36225a1d7ce282e14
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-06 12:49:06 +0530

    Area of circle (first question)

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 //      formula   3.14*r*r
        float area;
       // we use final for the value of pi does not change
-        final float pi=3.14f;// and dont forget to write a we use use float use should use (f)
+        final float pi=3.14f;// and dint't forget to write a we use use float use should use (f)
         float radius=2f;
         area=pi*radius*radius;
         System.out.println("area of circle " + area);
