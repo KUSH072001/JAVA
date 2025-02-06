9b15d134ff2e651d73d62df3faad23fe8a1f6cfc
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-06 16:07:10 +0530

    question 4

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
 
+//        Write a Java program to get a number from the user and print whether it is
+//        positive or negative.
 
-       int x = 200, y = 50, z = 100;
-if(x > y && y > z){
-System.out.println("Hello");
-}
-if(z > y && z < x){
-System.out.println("Java");
-}
-if((y+200) < x && (y+150) < z){
-System.out.println("Hello Java");
-}
-
-
-
-
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int number=sc.nextInt();
+                if(number>0){
+                    System.out.println(number+" number is positive");
+                } else if (number<0) {
+                    System.out.println(number+" number is negative");
+                }
     }
 }
