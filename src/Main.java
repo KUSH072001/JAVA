84ef98ba60b0b664ad95ec7ef9a82926a95acf0a
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-06 19:36:12 +0530

    reverse star printed

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 //        printed star pattern
      for(int i=0;i<=4;i++){
-         for(int j=0;j<=i;j++){
+         for(int j=4;j>=i;j--){
              System.out.print("*");
          }
          System.out.println(" ");
