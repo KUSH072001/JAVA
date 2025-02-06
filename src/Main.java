73ad4f976d01a1ecbf575543c93cc11579912e90
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-06 19:50:12 +0530

    pyramid pattern 2

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,19 @@
 public class Main {
     public static void main(String[] args) {
 
-//        1
-//        22
-//        333
-//        4444
-//        55555
+//   1
+//12
+//123
+//1234
+//12345
 
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(i);
+            for(int i=1;i<=5;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(j);
+                }
+                System.out.println(" ");
             }
-            System.out.println(" ");
-        }
+
 
 
     }
