bd4c981faaaad85f901e3d1138c61ba07cb6b816
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-06 19:48:17 +0530

    pyramid pattern

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,18 @@
 public class Main {
     public static void main(String[] args) {
 
+//        1
+//        22
+//        333
+//        4444
+//        55555
 
-//        printed star pattern
-     for(int i=0;i<=4;i++){
-         for(int j=4;j>=i;j--){
-             System.out.print("*");
-         }
-         System.out.println(" ");
-     }
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println(" ");
+        }
 
 
     }
