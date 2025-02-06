ea823d1beab7d3c8007bb8f6bd7ad18d64c2ba26
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-06 13:00:00 +0530

    Average of two number user input

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-//      Area of circle
-//      formula   3.14*r*r
-       float area;
-      // we use final for the value of pi does not change
-        final float pi=3.14f;// and dint't forget to write a we use use float use should use (f)
-        float radius=2f;
-        area=pi*radius*radius;
-        System.out.println(" area of circle " + area);
+//        type conversion
 
+        Scanner sc=new Scanner(System.in);
+            float Average;
+        System.out.println("Enter Number N1");
+        int n1=sc.nextInt();
+        System.out.println("Enter Number N2");
+        int n2=sc.nextInt();
+
+        Average=(n1+n2)/2f;
+        System.out.println(Average);
 
     }
 }
