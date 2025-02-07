08715da77a0faada756a91e5261a8bb55d421010
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-07 23:54:36 +0530

    multipal object creation

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,24 @@ public class Main {
 
 
 
-        Student st=new Student( );
-        System.out.println(st.name);
-        System.out.println(st.age);
+        Student1 st1=new Student1( );
+        Student2 st2=new Student2( );
+        System.out.println(st1.name);
+        System.out.println(st1.age);
+     System.out.println(st2.name);
+        System.out.println(st2.age);
     }
 }
 
 
-class Student{
+class Student1{
 
     String name="krishna";
     int age=12;
 
 
+}
+class Student2{
+    String name="manoj";
+    int age=12;
 }
