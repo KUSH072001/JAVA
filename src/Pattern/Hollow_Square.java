40d8a34eec2d9d04fb0e3289cb669708969323b2
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-09 23:37:15 +0530

    Inverted rotated half pyramid

--- src/Pattern/Hollow_Square.java
+++ src/Pattern/Hollow_Square.java
@@ -1,32 +1,32 @@
 package Pattern;
 
-class Pattern{
-    void method(int ttl_row, int ttl_col){
-        for(int i=1;i<=ttl_row;i++){
-            for(int j=1;j<=ttl_col;j++){
-                if(i==1 || i== ttl_row || j==1 || j==ttl_col ){
-                    System.out.print("*");
-                }else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println(" ");
-        }
-    }
-}
+
 
 public class Hollow_Square {
     public static void main(String[] args) {
 //        hollow square pattern
-//        ****
-//        *  *
-//        *  *
-//        ****
+//
+//          *
+//          **
+//          ***
+//          ****
+//
 //
 
-Pattern p1=new Pattern();
-p1.method(4,4);
 
+       for(int i=1;i<=4;i++){
+           for(int j=4;j>=i;j--){
+              for(int k=1;k<=i;k++){
+                  if(j==i) {
+                      System.out.print("*");
+                  }
+              }
+
+                  System.out.print(" ");
+           }
+           System.out.println(" ");
+
+       }
 
 
     }
