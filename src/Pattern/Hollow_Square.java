efcd42304a9a59296e5fcc50ece358f11d931d2b
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-10 12:32:47 +0530

    pattern

--- src/Pattern/Hollow_Square.java
+++ src/Pattern/Hollow_Square.java
@@ -5,10 +5,10 @@ package Pattern;
 public class Hollow_Square {
     public static void main(String[] args) {
 
-//        1
-//        12
-//        123
-//        1234
+//4
+//43
+//432
+//4321
 
         for( int i=4;i>=1;i--){
     for(int j=4;j>=i;j--){
