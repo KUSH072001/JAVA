17f99693920a1c11b07f162400a6b339c32278e6
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-10 17:29:41 +0530

    pattern

--- src/Pattern/Hollow_Square.java
+++ src/Pattern/Hollow_Square.java
@@ -4,28 +4,18 @@ package Pattern;
 
 public class Hollow_Square {
     public static void main(String[] args) {
-///1
-//        12
-//        123
-//        1234
-//        12345
-//        1234
-//        123
-//        12
-//        1
-
-       for(int i=1;i<=5;i++) {
-           for (int j = 1; j <= i; j++) {
-               System.out.print(j);
-           }
-           System.out.println(" ");
-
-           }
-        for (int x = 3; x >= 1; x--) {
-            for (int y = 1; y <=x; y++) {
-                System.out.print(y);
-            }
-            System.out.println(" ");
+///
+//1    1
+//23
+//456
+//78910
+    int k=1;
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(k);
+            k++;
         }
-       }
+        System.out.println(" ");
+    }
+    }
 }
