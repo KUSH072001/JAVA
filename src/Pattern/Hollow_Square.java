f20f3f1db9ed690ee149617fc9b68bfce9e6d15f
krishna deshmukh <krishnadesh2001@gmail.com>
2025-02-10 12:18:34 +0530

    pattern

--- src/Pattern/Hollow_Square.java
+++ src/Pattern/Hollow_Square.java
@@ -7,13 +7,12 @@ public class Hollow_Square {
 
 
 
-      for(int i=4;i>=1;i--){
-          for(int j=i;j>=1;j--){
-              System.out.print(j);
+      for(int i=1;i<=4;i++){
+          for(int j=1;j<=i;j++){
+              System.out.print(i);
           }
           System.out.println(" ");
       }
-
 
 
     }
