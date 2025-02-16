04367a5b3e94ee76cf6009010371f907e3ef16ec
Krishna Deshmukh <krishnadesh2001@gmail.com>
2025-02-16 13:30:41 +0530

    testing

--- src/Test01/main.java
+++ src/Test01/main.java
@@ -4,6 +4,6 @@ public class main extends A{
     public static void main(String[] args) {
         main m1=new main();
         System.out.println(m1.a);
-        System.out.println("hello   world");
+        System.out.println("hello world");
     }
 }
