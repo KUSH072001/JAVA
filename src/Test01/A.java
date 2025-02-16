e6b68f653949233a7a97b01a9dec4e4d5b2a385a
Krishna Deshmukh <krishnadesh2001@gmail.com>
2025-02-16 13:30:25 +0530

    testing

---
+++ src/Test01/A.java
@@ -0,0 +1,5 @@
+package Test01;
+
+public class A {
+    int a=10;
+}
--- src/Test01/main.java
+++ src/Test01/main.java
@@ -1,7 +1,9 @@
 package Test01;
 
-public class main {
+public class main extends A{
     public static void main(String[] args) {
+        main m1=new main();
+        System.out.println(m1.a);
         System.out.println("hello   world");
     }
 }
